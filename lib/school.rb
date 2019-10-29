6cd11c2b82a1155a89542ca979de49f00fbc358f
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-29 20:48:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] = Array.new
+    @roster[grade] << name
+  end
+  
+end
